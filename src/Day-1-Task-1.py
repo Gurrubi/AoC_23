9a2d795d8bc66804c9ce08a9c519a62f403c3942
Gurrubi <victorguti0@gmail.com>
2023-12-01 17:55:33 +0100

    Day1

---
+++ src/Day-1-Task-1.py
@@ -0,0 +1,22 @@
+file = open("../inputs/input-Day-1-Task-1.txt", "r")
+
+last = first = "0"
+first_num = True
+sum = 0
+
+for line in file:
+    for character in line:
+        if (character.isdigit()):
+            if(first_num):
+                first = character
+                first_num = False
+
+            last = character
+
+    sum += int(first+last)
+    first_num = True
+    first = last = "0"
+
+file.close();
+
+print(sum)
